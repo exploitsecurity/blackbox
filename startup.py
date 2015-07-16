b5e06926a1290697c8e0f54eab7e3c80dc9c7ed7
exploitsecurity <sy@exploit-security.co.uk>
2015-07-16 10:46:20 +0100

    Wireless card detection
    Detect Wireless card

--- startup.py
+++ startup.py
@@ -8,6 +8,7 @@ pins = [18, 23]
 
 def main():
     init()
+    initWireless()
     
 def init():
     print '[*] Initiate LEDs [*]'
@@ -20,12 +21,38 @@ def init():
     print '[*] Process Complete [*]'
     
 def initWireless():
+    print '[*] Checking for Wireless Card [*]'
+    
+    os.system('iwconfig > iwconfig.txt')
+    
+    if int(os.path.getsize('iwconfig')) > 0:
+        file = open('iwconfig.txt', 'r')
+        for line in file.readlines():
+            if 'wlan' in line:
+                print '[*] Wireless Card Detected [*]'
+                file.close()
+                activeWireless()
+            else:
+                print '[!] No Wireless Card Detected [!]'
+                file.close()
+                errorWireless()
+            break
+    else:
+        print '[!] No Wireless Card Detected [!]'
+        file.close()
+        errorWireless()
 
 def activeWireless():
+    delete('iwconfig.txt')
+    print 'hsudsj'
 
 def errorWireless():
+    delete('iwconfig.txt')
+    print 'dsfsfd'
 
-def errorWiFi():
+
+def delete(c):
+    os.system('rm ' + c)
 
 if __name__ == '__main__':
     main()
